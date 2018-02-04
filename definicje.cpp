2c7ad700ae84830ade21415293b5c2dd37fa4e12
machas1337 <machas1337@users.noreply.github.com>
2018-02-04 16:52:56 +0100

    Delete definicje.cpp

--- definicje.cpp
+++
@@ -1,7 +0,0 @@
-#define PUNKT_ZERO_X -200
-#define PUNKT_ZERO_Y 100
-#define PUNKT_ZERO_YP 93 //punkt poczatkowy rysowania od procesorow (pierwsza kreska PROC. 1)
-#define ODLEGLOSC_PROCESOR -(250/9)
-#define ODLEGLOSC_CZAS (400/11) //mnożnik musi znajdować się z przodu, np: i*ODLEGLOSC_CZASU
-#define RECT_SIZE_W 36
-#define RECT_SIZE_H -27 //wielkosci pojedyńczego zadania
